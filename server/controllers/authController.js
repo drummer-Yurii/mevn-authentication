5e4027bdd5cdc72486762131000bf759a216b15b
Yurii Rybachok <drummer2009@ukr.net>
2024-10-04 18:43:45 +0300

    add logic for register controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,8 +1,26 @@
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
-const register = (req, res) => {
-  res.sendStatus(200);
+const register = async (req, res) => {
+  const { username, email, first_name, last_name, password, password_confirm } = req.body;
+  if (!username || !email || !password || !password_confirm || !first_name || !last_name) {
+    return res.status(422).json({ message: 'Invalid fields' });
+  }
+  if (password !== password_confirm) {
+    return res.status(422).json({ message: 'Password do not match' });
+  }
+  const userExists = await User.exists({ email }).exec();
+  if (userExists) return res.status(409);
+  try {
+    hashedPassword = await bcrypt.hash(password, 10);
+    await User.create({ email, username, password: hashedPassword, first_name, last_name });
+    return res.sendStatus(201);
+  } catch (error) {
+    return res.status(400).json({ message: 'Could not register' });
+  }
 };
+
 const login = (req, res) => {
   res.sendStatus(200);
 };
