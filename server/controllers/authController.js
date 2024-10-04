6701bcca8945e9512992c6f388c6ea7c7e34fc68
Yurii Rybachok <drummer2009@ukr.net>
2024-10-04 16:31:44 +0300

    initialize authentication controller

---
+++ server/controllers/authController.js
@@ -0,0 +1,17 @@
+const register = (req, res) => { 
+    
+}
+const login = (req, res) => { 
+
+}
+const logout = (req, res) => { 
+
+}
+const refresh = (req, res) => { 
+
+}
+const user = (req, res) => { 
+
+}
+
+module.exports = {register, login, logout, refresh, user};
--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const authControllers = require('../../controllers/authController');
 
-router.post('/register');
-router.post('/login');
-router.post('/logout');
-router.post('/refresh');
-router.get('/user');
+router.post('/register', authControllers.register);
+router.post('/login', authControllers.login);
+router.post('/logout', authControllers.logout);
+router.post('/refresh', authControllers.refresh);
+router.get('/user', authControllers.user);
 
 module.exports = router;
