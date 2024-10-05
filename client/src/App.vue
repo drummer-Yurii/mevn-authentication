96f221ea87fc62d84fc6a46b821a91a59e36742b
Yurii Rybachok <drummer2009@ukr.net>
2024-10-05 18:40:39 +0300

    add bootstrap & create navbar component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import Navbar from '@/components/Navbar.vue'
 </script>
 
 <template>
   <div id="app">
+    <Navbar />
     <RouterView />
   </div>
 </template>
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+      <router-link :to="{name: 'home'}" class="navbar-brand">Navbar</router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#appNavbar"
+        aria-controls="appNavbar"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="appNavbar">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link :to="{ name: 'home' }" class="nav-link" aria-current="page">
+                Home
+            </router-link>
+          </li>
+        </ul>
+        <ul class="navbar-nav mx-2 mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link :to="{ name: 'login' }" class="nav-link" aria-current="page">
+                Login
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link :to="{ name: 'register' }" class="nav-link" aria-current="page">
+                Register
+            </router-link>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              User
+            </a>
+            <ul class="dropdown-menu">
+              <li><router-link :to="{name: 'user'}" class="dropdown-item">Username</router-link></li>
+              <li><hr class="dropdown-divider" /></li>
+              <li><button class="dropdown-item btn btn-danger">Logout</button></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped></style>
