7a65d90eb3c430d91c2a4b6023c3d090cf1ed442
Yurii Rybachok <drummer2009@ukr.net>
2024-10-06 14:54:26 +0300

    handle user data in user view

--- client/src/views/auth/UserView.vue
+++ client/src/views/auth/UserView.vue
@@ -1,13 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useAuthStore } from '@/stores/auth'
+import { computed } from 'vue'
+
+const authStore = useAuthStore()
+
+const user = computed(() => {
+  return authStore.user
+})
+</script>
 
 <template>
   <div id="user">
     <div class="container">
-      <div class="card card-body mt-4">
-        <h5 class="card-title">Username: John</h5>
-        <h6 class="card-subtitle mb-2 text-muted">Email: john@mail.com</h6>
-        <h6 class="card-subtitle mb-2 text-muted">First Name: John</h6>
-        <h6 class="card-subtitle mb-2 text-muted">Last Name: Doe</h6>
+      <div v-if="user" class="card card-body mt-4">
+        <h5 class="card-title">Username: {{ user.username }}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Email: {{ user.email }}</h6>
+        <h6 class="card-subtitle mb-2 text-muted">First Name: {{ user.first_name }}</h6>
+        <h6 class="card-subtitle mb-2 text-muted">Last Name: {{ user.last_name }}</h6>
       </div>
     </div>
   </div>
