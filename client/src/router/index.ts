6d093fe9a86e746e16cfdcde88080b0bbe410db9
Yurii Rybachok <drummer2009@ukr.net>
2024-10-06 16:19:52 +0300

    creating navigation guards

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import { useAuthStore } from '@/stores/auth'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,19 +13,31 @@ const router = createRouter({
     {
       path: '/login',
       name: 'login',
-      component: () => import('../views/auth/LoginView.vue')
+      component: () => import('../views/auth/LoginView.vue'),
+      meta: { requiresGuest: true }
     },
     {
       path: '/register',
       name: 'register',
-      component: () => import('../views/auth/RegisterView.vue')
+      component: () => import('../views/auth/RegisterView.vue'),
+      meta: { requiresGuest: true }
     },
     {
       path: '/user',
       name: 'user',
-      component: () => import('../views/auth/UserView.vue')
+      component: () => import('../views/auth/UserView.vue'),
+      meta: { requiresAuth: true }
     }
   ]
+})
+
+router.beforeEach((to) => {
+  const store = useAuthStore()
+  if (to.meta.requiresAuth && !store.isAuthenticated) {
+    return { name: 'login', query: { redirect: to.fullPath } }
+  } else if (to.meta.requiresGuest && store.isAuthenticated) {
+    return { name: 'home' }
+  }
 })
 
 export default router
